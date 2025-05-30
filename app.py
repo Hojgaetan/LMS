26809b660744397ce145a79ee2a8f3fa75ac411d
Hojgaetan <joel-gaetan.hassam@ism.edu.sn>
2025-05-30 10:05:22 +0200

    Refactor app structure by removing unused code and adding navbar template; enhance dashboard layout

--- app.py
+++ app.py
@@ -1,41 +1,16 @@
-from flask import Flask, render_template, request, redirect, url_for, jsonify
-from models.book import Book
-from models.author import Author
-from models.category import Category
-from models.member import Member
+from flask import Flask
+from controllers.book_controller import book_blueprint
+from controllers.member_controller import member_blueprint
+from controllers.dashboard_controller import dashboard_blueprint
 
-app = Flask(__name__)
-
-# Importer les blueprints
-@app.route('/', methods=['GET'])
-def index():
-    try:
-        # Récupérer les statistiques
-        total_books = Book.count()
-        total_category = Category.count()
-        total_authors = Author.count()
-        total_members = Member.count()
-        total_popular_books = Book.count_popular_books(threshold=10)
-        total_active_members = Member.count_active_members()
-        total_overdue_books = Book.count_overdue_books()
-        #popular_books = Book.get_popular_books(limit=5)
-        #active_members = Member.get_active_members(limit=5)
-        #overdue_books = Book.get_overdue_books()
-    except Exception as e:
-        return jsonify({'error': str(e)}), 500
-    else:
-        return render_template('index.html', 
-                           total_books=total_books, 
-                           total_category=total_category,
-                           total_authors=total_authors, 
-                           total_members=total_members,
-                           total_popular_books=total_popular_books,
-                           total_active_members= total_active_members,
-                           total_overdue_books=total_overdue_books,
-                           #popular_books=popular_books, 
-                           #active_members=active_members, 
-                           #overdue_books=overdue_books
-                           )
+def create_app():
+    app = Flask(__name__)
+    app.register_blueprint(book_blueprint, url_prefix='/books')
+    app.register_blueprint(member_blueprint, url_prefix='/members')
+    app.register_blueprint(dashboard_blueprint, url_prefix='/')
+    return app
 
 if __name__ == '__main__':
+    app = create_app()
     app.run(debug=True)
+    
